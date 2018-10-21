90e3f28191c9bc6b3c4ab8936e8bbea63b6745d2
Pavel Sh <pavel.Sheinfeld@gmail.com>
2018-10-21 12:23:57 +0300

    graph

--- tester/index.js
+++ tester/index.js
@@ -1,16 +1,21 @@
-module.exports = async function (context, req) {
+module.exports = async function (context, req, graphToken) {
     context.log('JavaScript HTTP trigger function processed a request.');
     //context.log(req.headers);
+    rsp = req.headers
+    rsp = rsp["graphToken"] = graphToken
+
     if (req.query.name || (req.body && req.body.name)) {
+
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: req.headers//"Hello " + (req.query.name || req.body.name)
+            
+            body: rsp//"Hello " + (req.query.name || req.body.name)
         };
     }
     else {
         context.res = {
             //status: 400,
-            body: req.headers 
+            body: rsp 
         };
     }
 };
