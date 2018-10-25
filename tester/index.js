ec82a2c13957b59322a82623e0548adbf6fc034b
Pavel Sh <pavel.Sheinfeld@gmail.com>
2018-10-25 11:42:55 +0300

    working version

--- tester/index.js
+++ tester/index.js
@@ -1,44 +1,43 @@
 const rp = require('request-promise');
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    //context.log(req.headers);
-    rsp = req.headers
+const profileExtractPropsList = ["id","givenName","surname","userPrincipalName"];
+
+module.exports = function (context, req) {
     let token = "Bearer " + context.bindings.graphToken;
+
     let options = {
         uri: 'https://graph.microsoft.com/v1.0/me/',
         headers: {
             'Authorization': token
         }
     };
-    
+
     rp(options)
         .then(function(profile) {
+            var objProfile = JSON.parse(profile);
+            // un-comment to see all properties in console
+            // Object.keys(objProfile).map(function(key,index){
+            //     context.log(key + " : " + objProfile[key]);
+            // });
+            var filtered = Object.keys(objProfile)
+                .filter(key => profileExtractPropsList.includes(key))
+                .reduce((obj, key) => {
+                    return {
+                        ...obj,
+                        [key]: objProfile[key]
+                    };
+                },
+            {});
             context.res = {
-                body: profile
+                body:filtered
             };
             context.done();
         })
         .catch(function(err) {
             context.res = {
                 status: 500,
-                body: err
+                body: {"error":"graph responce error","fullError":err}
             };
             context.done();
         });
-
-    // if (req.query.name || (req.body && req.body.name)) {
-
-    //     context.res = {
-    //         // status: 200, /* Defaults to 200 */
-            
-    //         body: rsp//"Hello " + (req.query.name || req.body.name)
-    //     };
-    // }
-    // else {
-    //     context.res = {
-    //         //status: 400,
-    //         body: rsp 
-    //     };
-    // }
 };
